5498dc4b3febff5130dea085e302814dd0110d2b
june29 <june29.jp@gmail.com>
2013-07-13 16:28:04 +0900

    Update scraper to catch with latest jigokuno HTML

--- lib/jigokuno.rb
+++ lib/jigokuno.rb
@@ -6,8 +6,8 @@ require "rubygems"
 require "nokogiri"
 
 module Jigokuno
-  URL        = "http://jigokuno.com/"
-  NEXT_XPATH = "//div[@id='page_area']/div[@class='page_navi']/a[contains(text(), '>>')]"
+  URL               = "http://jigokuno.com/"
+  NEXT_CSS_SELECTOR = "#pager span.pager_next a"
 
   class Misawa
     include Enumerable
@@ -20,7 +20,7 @@ module Jigokuno
     end
 
     def next
-      next_link = @document.at(NEXT_XPATH)
+      next_link = @document.at(NEXT_CSS_SELECTOR)
 
       return nil if next_link.nil?
 
@@ -47,25 +47,41 @@ module Jigokuno
 
     def meigens
       @html.xpath("//div[@class='entry_area']").each { |entry|
-        h2 = entry.xpath(".//h2").text.tr("０-９", "0-9").chomp
-        id, title = h2.scan(/惚れさせ(\d+).*「(.+)」/).first
+        h2 = entry.at(".//h2")
+        category_anchor = entry.at(".//ul[@class='state']/li[2]/a")
 
-        id        = id.to_i
-        title     = title.to_s
-        image     = entry.at(".//img[@class='pict']").attributes["src"].to_s
-        character = entry.at("center/ul/li[2]/a").text
-        cid       = entry.at("center/ul/li[2]/a").attributes["href"].to_s.slice(/cid=(.+)/,1).to_i
-        eid       = entry.at("center/ul/li[3]/a").attributes["href"].to_s.slice(/eid=(.+)/,1).to_i
+        permalink_of_category = category_anchor.attribute("href").value
+        permalink_of_entry = h2.at("./a/@href").value
+        id_str, title = h2.text.scan(/惚れさせ([０-９]+).*「(.+)」/).first
 
-        yield Hash[{
-          :id        => id,
-          :title     => title,
-          :image     => image,
-          :character => character,
-          :cid       => cid,
-          :eid       => eid
-        }]
+        scraped = {
+          id:        id_str.tr("０-９", "0-9").to_i,
+          title:     title,
+          image:     entry.at(".//img[@class='pict']/@src").value,
+          character: category_anchor.text,
+          cid:       extract_cid(permalink_of_category),
+          eid:       extract_eid(permalink_of_entry)
+        }
+
+        yield scraped
       }
+    end
+
+    private
+    def extract_eid(url)
+      if matched = url.match(/eid=(\d+)\z/)
+        matched[1].to_i
+      else
+        raise "Can't extract eid from #{url}"
+      end
+    end
+
+    def extract_cid(url)
+      if matched = url.match(/cid=(\d+)\z/)
+        matched[1].to_i
+      else
+        raise "Can't extract cid from #{url}"
+      end
     end
   end
 end
